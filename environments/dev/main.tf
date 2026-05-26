2e0a91c304485921968f51fea083669bdc139924
root <root@ip-10-0-13-244.ec2.internal>
2026-05-26 09:10:06 +0000

    all change added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -5,3 +5,18 @@ module "ec2" {
   instance_type = var.instance_type
   env           = var.env
 }
+
+module "rds" {
+  source = "../../modules/rds"
+
+  env                = var.env
+  allocated_storage  = var.allocated_storage
+  engine             = var.engine
+  engine_version     = var.engine_version
+  instance_class     = var.instance_class
+  db_username        = var.db_username
+  db_password        = var.db_password
+  db_name            = var.db_name
+  subnet_ids         = var.subnet_ids
+  security_group_ids = var.security_group_ids
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,26 @@
 variable "ami_id" {}
 variable "instance_type" {}
 variable "env" {}
+
+variable "allocated_storage" {}
+variable "engine" {}
+
+variable "engine_version" {}
+
+variable "instance_class" {}
+
+variable "db_username" {}
+
+variable "db_password" {
+  sensitive = true
+}
+
+variable "db_name" {}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "security_group_ids" {
+  type = list(string)
+}
