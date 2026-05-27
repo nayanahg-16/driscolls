d6a4b961a1cc8945a7316a5dd475a43ac3ab5220
root <root@ip-10-0-13-244.ec2.internal>
2026-05-27 12:03:25 +0000

    file added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,13 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  env                    = var.env
+  vpc_cidr               = var.vpc_cidr
+  public_subnet_cidr_1   = var.public_subnet_cidr_1
+  public_subnet_cidr_2   = var.public_subnet_cidr_2
+  az_1                   = var.az_1
+  az_2                   = var.az_2
+}
 module "ec2" {
   source = "../../modules/ec2"
 
@@ -34,3 +44,34 @@ module "ecs" {
   subnet_ids         = var.subnet_ids
   security_group_ids = var.security_group_ids
 }
+
+
+
+module "alb" {
+  source = "../../modules/alb"
+  env    = var.env
+  vpc_id = module.vpc.vpc_id
+
+  subnet_ids = [
+    module.vpc.public_subnet_1,
+    module.vpc.public_subnet_2
+  ]
+}
+
+
+module "autoscaling" {
+  source       = "../../modules/autoscaling"
+  cluster_name = module.ecs.cluster_name
+  service_name = module.ecs.service_name
+}
+
+module "waf" {
+  source  = "../../modules/waf"
+  env     = var.env
+  alb_arn = module.alb.alb_arn
+}
+
+#module "cloudfront" {
+  source  = "../../modules/cloudfront"
+  alb_dns = module.alb.alb_dns
+  #}#
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,12 @@
+variable "vpc_cidr" {}
+variable "public_subnet_cidr_1" {}
+
+variable "public_subnet_cidr_2" {}
+
+variable "az_1" {}
+
+variable "az_2" {}
+
 variable "ami_id" {}
 variable "instance_type" {}
 variable "env" {}
