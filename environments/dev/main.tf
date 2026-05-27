7ebf7a880aed28d6fbdc5ca174338c1ded27368f
root <root@ip-10-0-13-244.ec2.internal>
2026-05-27 06:14:53 +0000

    added

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -20,3 +20,17 @@ module "rds" {
   subnet_ids         = var.subnet_ids
   security_group_ids = var.security_group_ids
 }
+module "ecs" {
+  source = "../../modules/ecs"
+
+  env                = var.env
+  aws_region         = var.aws_region
+  cpu                = var.cpu
+  memory             = var.memory
+  container_image    = var.container_image
+  container_port     = var.container_port
+  desired_count      = var.desired_count
+  execution_role_arn = var.execution_role_arn
+  subnet_ids         = var.subnet_ids
+  security_group_ids = var.security_group_ids
+}
--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -24,3 +24,18 @@ variable "subnet_ids" {
 variable "security_group_ids" {
   type = list(string)
 }
+
+variable "aws_region" {}
+
+variable "cpu" {}
+
+variable "memory" {}
+
+variable "container_image" {}
+
+variable "container_port" {}
+
+variable "desired_count" {}
+
+variable "execution_role_arn" {}
+
